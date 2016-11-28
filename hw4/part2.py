d2d70e2e35535ef95b30f4fd4753d7ab515bcb34
rosalyntan <rtan@nd.edu>
2016-11-28 13:06:38 -0600

    over 88% F1 score for CKY parser

--- hw4/part2.py
+++ hw4/part2.py
@@ -1,7 +1,8 @@
 # Rosalyn Tan
 # CKY Parser
 
-from queue import PriorityQueue
+import math
+import numpy as np
 from tree import Tree
 
 rules = {} # maps from left of rule to right of rule to frequency
@@ -34,21 +35,12 @@ def read_trees():
 				rules[node.label][right] = 1
 
 def count_rules():
-	q = PriorityQueue()
 	unique_rules = 0
 	for rule in rules:
 		for right in rules[rule]:
 			unique_rules += 1
-			q.put((-rules[rule][right], rule + ' -> ' + right))
-
-	print('Number of unique rules: %d' % unique_rules)
-	print('Top 5 most frequent rules:')
-	for i in range(0, 5):
-		freq = q.get()
-		print(str(freq[1]) + ' # ' + str(freq[0]*-1))
 
 def cond_prob():
-	q = PriorityQueue()
 	left_count = 0
 	for rule in rules:
 		prob[rule] = {}
@@ -56,14 +48,7 @@ def cond_prob():
 			left_count += rules[rule][right]
 		for right in rules[rule]:
 			prob[rule][right] = rules[rule][right] / left_count
-			q.put((-prob[rule][right], rule + ' -> ' + right))
-#			print(rule + ' -> ' + right + ' # ' + str(prob[rule][right]))
 		left_count = 0
-	
-	print('Top 5 highest probability rules:')
-	for i in range(0, 5):
-		p = q.get()
-		print(str(p[1]) + ' # ' + str(p[0]*-1))
 
 def viterbi_cky(string):
 	s = string.split()
@@ -80,6 +65,7 @@ def viterbi_cky(string):
 			back[i][j] = {}
 			for rule in rules:
 				best[i][j][rule] = 0
+#				best[i][j][rule] = -np.inf
 				back[i][j][rule] = []
 
 	# terminal probabilities
@@ -90,6 +76,8 @@ def viterbi_cky(string):
 			word = '<unk>'
 		for left in terminals[word]:
 			if prob[left][word] > best[i-1][i][left]:
+#			if math.log(prob[left][word]) > best[i-1][i][left]:
+#				best[i-1][i][left] = math.log(prob[left][word])
 				best[i-1][i][left] = prob[left][word]
 				back[i-1][i][left].append(word)
 
@@ -104,20 +92,28 @@ def viterbi_cky(string):
 						if len(r) == 1:
 							continue
 						else:
-							p = rules[rule][right] * best[i][k][r[0]] * best[k][j][r[1]]
+							p = prob[rule][right] * best[i][k][r[0]] * best[k][j][r[1]]
+#							p = math.log(prob[rule][right]) + best[i][k][r[0]] + best[k][j][r[1]]
 							if p > best[i][j][rule]:
 								best[i][j][rule] = p
+								back[i][j][rule] = []
 								back[i][j][rule].append(r[0])
 								back[i][j][rule].append(r[1])
 								back[i][j][rule].append(k)
-
-	print_tree('TOP', i, j, back)
+	if back[0][len(s)]['TOP'] == []:
+		print('', end = '')
+	else:
+		print_tree('TOP', 0, len(s), back)
 
 def print_tree(rule, i, j, back):
 	if len(back[i][j][rule]) == 1:
 		print('(' + rule + ' ' + back[i][j][rule][0] + ')', end = '')
 	else:
-		print('(' + str(rule) + ' ' + str(print_tree(back[i][j][rule][0], i, back[i][j][rule][2], back)) + ' ' + str(print_tree(back[i][j][rule][1], back[i][j][rule][2], j, back)) + ')', end = '')
+		print('(' + rule + ' ', end = '')
+		print_tree(back[i][j][rule][0], i, back[i][j][rule][2], back)
+		print(' ', end = '')
+		print_tree(back[i][j][rule][1], back[i][j][rule][2], j, back)
+		print(')', end = '')
 
 if __name__ == '__main__':
 	read_trees()
